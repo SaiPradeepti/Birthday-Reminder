4799e8908426bc07377ee3f953f42290c9969d0f
SaiPradeepti <saipradeepti14@gmail.com>
2020-11-28 23:54:23 +0530

    final commit

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <section className="container">
         <h3>{people.length} birthdays today</h3>
         <List people={people} />
-        <button className="btn" onClick={() => console.log('clicked')}>clear all</button>
+        <button className="btn" onClick={() => setPeople([])}>clear all</button>
       </section>
     </main>
   );
